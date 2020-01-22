9d6a1c4d411fc75e177daa8d8bca5ea479380d39
zoubo0690@163.com <zoubo0690@163.com>
2020-01-22 10:55:59 +0000

    回复数据添加

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -51,4 +51,9 @@ class Topic extends Model
         }
         return route('topics.show', array_merge([$this->id, $slug], $params));
     }
+    // 关联回复
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,9 +43,14 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
-
+    // 权限判断
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
+    }
+    // 关联回复
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
