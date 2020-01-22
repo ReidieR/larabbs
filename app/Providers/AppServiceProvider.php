bcb86da6a99bad195ee4df889ba057b22940e7c9
zoubo0690@163.com <zoubo0690@163.com>
2020-01-22 14:23:49 +0000

    消息通知有新通知

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,5 @@ class AppServiceProvider extends ServiceProvider
         // \App\Models\User::observe(\App\Observers\UserObserver::class);
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-        //
     }
 }
